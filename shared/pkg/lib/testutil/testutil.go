51c0f976283f7ab674809ddbcfae94a36af7d653
Enrico Borba <enricozb@gmail.com>
2021-02-27 22:34:42 -1000

    testutil

---
+++ shared/pkg/lib/testutil/testutil.go
@@ -0,0 +1,42 @@
+package testutil
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/Masterminds/squirrel"
+
+	"github.com/enricozb/pho/shared/pkg/effects/db"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+)
+
+func MockDB(t *testing.T) (mockDB *sqlx.DB, cleanup func()) {
+	assert := assert.New(t)
+
+	tmpdir, err := ioutil.TempDir("", "pho-tests-")
+	assert.NoError(err, "create tempdir")
+
+	mockDB, err = db.NewDB(tmpdir, "mock-db")
+	assert.NoError(err, "new mock db")
+
+	assert.NoError(db.Migrate(mockDB), "migrate mock db")
+
+	return mockDB, func() { os.RemoveAll(tmpdir) }
+}
+
+func NumRows(t *testing.T, db *sqlx.DB, tableName string) (count int) {
+	assert := assert.New(t)
+
+	q, args, err := squirrel.
+		Select("count(*)").
+		From(tableName).
+		ToSql()
+
+	assert.NoError(err, "build count query")
+
+	assert.NoError(db.Get(&count, q, args...), "query count")
+	return count
+}
