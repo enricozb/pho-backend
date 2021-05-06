bf22fe7207d77051cd71886908cac9dd983cf0ac
Enrico Borba <enricozb@gmail.com>
2021-05-05 21:19:31 -1000

    job_test -> jobs_test

--- shared/pkg/effects/daos/jobs/jobs_test.go
+++ shared/pkg/effects/daos/jobs/jobs_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 
+	"github.com/enricozb/pho/shared/pkg/effects/daos/jobs"
 	"github.com/enricozb/pho/shared/pkg/lib/testutil"
 )
 
@@ -14,4 +15,27 @@ func setup(t *testing.T) (*require.Assertions, *gorm.DB, func()) {
 	db, cleanup := testutil.MockDB(t)
 
 	return assert, db, cleanup
+}
+
+func TestJobs_JobSmokeTest(t *testing.T) {
+	assert, db, cleanup := setup(t)
+	defer cleanup()
+
+	insertedImport := jobs.Import{}
+	db.Create(&insertedImport)
+
+	insertedJob := jobs.Job{ImportID: insertedImport.ID, Kind: jobs.JobScan, Args: []byte("{}")}
+	db.Create(&insertedJob)
+
+	foundJob := jobs.Job{ImportID: insertedImport.ID}
+	assert.NoError(db.First(&foundJob).Error)
+
+	assert.Equal(jobs.JobStatusNotStarted, insertedJob.Status)
+	assert.Equal(insertedJob, foundJob)
+
+	jobs := []jobs.Job{}
+	assert.NoError(db.Where("import_id = ?", insertedImport.ID.String()).Find(&jobs).Error)
+
+	assert.Len(jobs, 1)
+	assert.Equal(insertedJob, jobs[0])
 }
