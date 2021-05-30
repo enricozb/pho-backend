e9491ab52c7e992397209d6a66cf5fe35866f9d3
Enrico Borba <enricozb@gmail.com>
2021-05-29 16:35:31 -1000

    remove old jobs

--- shared/pkg/effects/daos/jobs/kind.go
+++ shared/pkg/effects/daos/jobs/kind.go
@@ -3,16 +3,14 @@ package jobs
 type JobKind string
 
 const (
-	JobScan              JobKind = "SCAN"
-	JobMetadata          JobKind = "METADATA"
-	JobMetadataHash      JobKind = "METADATA_HASH"
-	JobMetadataTimestamp JobKind = "METADATA_TIMESTAMP"
-	JobMetadataLive      JobKind = "METADATA_LIVE"
-	JobMetadataEXIF      JobKind = "METADATA_EXIF"
-	JobMetadataMonitor   JobKind = "METADATA_MONITOR"
-	JobDedupe            JobKind = "DEDUPE"
-	JobConvert           JobKind = "CONVERT"
-	JobCleanup           JobKind = "CLEANUP"
+	JobScan            JobKind = "SCAN"
+	JobMetadata        JobKind = "METADATA"
+	JobMetadataHash    JobKind = "METADATA_HASH"
+	JobMetadataEXIF    JobKind = "METADATA_EXIF"
+	JobMetadataMonitor JobKind = "METADATA_MONITOR"
+	JobDedupe          JobKind = "DEDUPE"
+	JobConvert         JobKind = "CONVERT"
+	JobCleanup         JobKind = "CLEANUP"
 )
 
 var MetadataJobKinds = []JobKind{
