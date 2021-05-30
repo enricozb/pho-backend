99ebfb5d76a5b2f9520a469265e4ef0c3fab77c0
Enrico Borba <enricozb@gmail.com>
2021-05-29 16:35:36 -1000

    add db initialization

--- shared/pkg/effects/db/db.go
+++ shared/pkg/effects/db/db.go
@@ -1,12 +1,17 @@
 package db
 
 import (
+	"fmt"
 	"path/filepath"
 	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"github.com/enricozb/pho/shared/pkg/effects/config"
 )
+
+const defaultDBFileName = "pho.db"
 
 func NewDB(dir, name string) (*gorm.DB, error) {
 	return gorm.Open(sqlite.Open(filepath.Join(dir, name)), &gorm.Config{
@@ -14,4 +19,12 @@ func NewDB(dir, name string) (*gorm.DB, error) {
 			return time.Now().UTC()
 		},
 	})
+}
+
+func MustDB() *gorm.DB {
+	if db, err := NewDB(config.Config.DBDir, defaultDBFileName); err != nil {
+		panic(fmt.Errorf("must db: %v", err))
+	} else {
+		return db
+	}
 }
