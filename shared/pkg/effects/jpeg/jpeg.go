94692d6a3231920eb96a89fe62dd8101d7cb96fe
Enrico Borba <enricozb@gmail.com>
2021-06-19 07:32:37 -1000

    increase thumbnail quality

--- shared/pkg/effects/jpeg/jpeg.go
+++ shared/pkg/effects/jpeg/jpeg.go
@@ -5,16 +5,16 @@ import (
 	"os/exec"
 )
 
-// Thumbnail creates a thumbnail of a `src` file (any image mimetype) with `dst` as the destination.
+// Thumbnail creates a thumbnail of a `src` file (any IMAGE file kind) with `dst` as the destination.
 // The aspect ratio is preserved.
 func Thumbnail(src, dst string) error {
 	// TODO(enricozb): add some check somewhere that if the size is less than X, just copy the file.
 
 	output, err := exec.Command(
 		"convert",
-		"-define", "jpeg:size=480x360",
+		"-define", "jpeg:size=960x720",
 		"-auto-orient",
-		"-thumbnail", "120x90",
+		"-thumbnail", "480x360",
 		"-strip",
 		src,
 		dst,
